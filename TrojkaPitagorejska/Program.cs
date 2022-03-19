1b0da916c3a1fa4bd9d026a1e6d07a85cc650017
KateIz <86878101+KateIz@users.noreply.github.com>
2022-03-19 14:48:01 +0100

    clean comments

--- TrojkaPitagorejska/Program.cs
+++ TrojkaPitagorejska/Program.cs
@@ -7,7 +7,7 @@ namespace TrojkaPitagorejska
               
         public double NumberCheck()
         {
-            //bool isNumber;
+            
             double a1;
             double n;
             do
@@ -36,56 +36,7 @@ namespace TrojkaPitagorejska
         {
             Program p = new Program();
             p.NumberCheck();
-
-            //do
-            //{
-            //    Console.WriteLine("Wprowadz a: ");
-            //    string a1 = (Console.ReadLine());
-            //    double.TryParse(a1, out double n);
-            //    if (n == 1)
-            //    {
-
-            //        double a = Convert.ToDouble(a1);
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Mozna wprowadzic tylko liczby: ");
-            //    }
-            //}
-            //while (n != 1);
-
-            
-            
-            
-            //do
-            //{
-            //    if (a <= 0)
-            //    {
-            //        Console.WriteLine("Mozna wprowadzic tylko dodatnia liczbe: ");
-            //        double a = Convert.ToDouble(Console.ReadLine())
-            //    }
-            //    else
-            //    {
-            //        break;
-            //    }
-            //}
-            //while (a <= 0);
-
-
-            //Console.WriteLine("Wprowadz b: ");
-            //double b = Convert.ToDouble(Console.ReadLine());
-            //Console.WriteLine("Wprowadz c: ");
-            //double c = Convert.ToDouble(Console.ReadLine());
-
-
-            //if (a * a + b * b == c * c)
-            //{
-            //    Console.WriteLine("Wprowadzone liczby sa trojka pitagoryjska");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Wprowadzone liczby nie sa trojka pitagoryjska");
-            //}
+           
 
         }
     }
