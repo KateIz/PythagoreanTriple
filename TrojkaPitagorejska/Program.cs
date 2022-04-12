8bf16dcd3ad86fc00fc0439aa1411d98b6f47a6c
KateIz <86878101+KateIz@users.noreply.github.com>
2022-04-12 10:33:26 +0200

    Updated Main

--- TrojkaPitagorejska/Program.cs
+++ TrojkaPitagorejska/Program.cs
@@ -5,39 +5,25 @@ namespace TrojkaPitagorejska
     class Program
     {
               
-        public double NumberCheck()
-        {
-            
-            double a1;
-            double n;
-            do
-            {
-                Console.WriteLine("Wprowadz a: ");
-                string a2 = (Console.ReadLine());
-                
-                double.TryParse(a2, out n);  
-                Console.WriteLine(n);
-                if ( n== 1)
-                {
-                   a1 = Convert.ToDouble(a2);
-                    return a1;
-                }
-                Console.WriteLine("Mozna wprowadzic tylko liczby: ");
-                           
-            }
-            while (n == 0);
-            Console.WriteLine("ok");            
-            return n;
-
-
-        }
-
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.NumberCheck();
-           
+            Console.WriteLine("Mozna wprowadzic tylko dodatnia liczbe:\nWprowadz a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wprowadz b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wprowadz c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
 
+
+            if (a * a + b * b == c * c)
+            {
+                Console.WriteLine("Wprowadzone liczby sa trojka pitagoryjska");
+            }
+            else
+            {
+                Console.WriteLine("Wprowadzone liczby nie sa trojka pitagoryjska");
+            }
+            Console.ReadKey();
         }
     }
 }
